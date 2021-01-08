dfce9c0f6a49080bab3921eb826db18dc3fc83f2
Tamas <mj.tamas@protonmail.com>
2021-01-08 09:10:16 +0200

    added post class and relations

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getAvatarAttribute()
+    {
+        return "https://i.pravatar.cc/40?u=". $this->email;
+    }
 }
